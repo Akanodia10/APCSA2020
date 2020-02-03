96c51cecca8ba540a8482c4ffdd157a429916b8e
arnavkanodia <arnavkanodia@localhost>
2020-02-02 22:43:47 -0800

    Variables done

--- APCS-A-2020/src/Unit0/Variables.java
+++ APCS-A-2020/src/Unit0/Variables.java
@@ -2,9 +2,9 @@ package Unit0;
 //(c) A+ Computer Science
 //www.apluscompsci.com
 
-//Name -
-//Date -
-//Class -
+//Name - Arnav Kanodia
+//Date - 1/31/20
+//Class -Period 2
 //Lab  -
 
 public class Variables
@@ -18,24 +18,44 @@ public class Variables
 		//char      boolean		String
 
 		//integer variables
+		
 		byte byteOne = 127;
-
-
-		//decimal variables
-
+        short shortOne = -12540;
+        int intOne = 332354240;
+        long longOne = 324932462;
+		
+        //decimal variables
+       
+        float floatOne = 0.34f;
+        double doubleOne=.324456986504;
 
 		//other integer types
-
+        
+        char charOne = 205;
 
 		//other types
-
+       
+        boolean boolOne = true;
+        String stringOne = "Arnav is in AP Comp Sci A";
 
 		//output your information here
 		System.out.println("/////////////////////////////////");
+		System.out.println("*Arnav Kanodia            2/2/20*");
 		System.out.println("*                               *");
 		System.out.println("*        integer types          *");
 		System.out.println("*                               *");
 		System.out.println("*8 bit - byteOne = "+byteOne+"\t\t*");
+		System.out.println("*16 bit - shortOne = "+shortOne+"\t\t");
+		System.out.println("*32 bit - intOne = "+intOne+"\t\t");
+		System.out.println("*64 bit - longOne = "+longOne+"\t\t");
+		System.out.println("*        real types             ");
+		System.out.println("*32 bit - floatOne = "+floatOne+"\t\t");
+		System.out.println("*64 bit - doubleOne = "+doubleOne+"\t\t");
+		System.out.println("*      other integer types      ");
+		System.out.println("*16 bit - charOne = "+charOne+"\t\t");
+		System.out.println("*        other types               ");
+		System.out.println("*boolOne = "+boolOne+"\t\t");
+		System.out.println("*stringOne = "+stringOne+"\t\t");
 
 
 
