ece74f7e9e3e84f77ec17d0aa1e462d0b67e55ed
arnavkanodia <arnavkanodia@localhost>
2020-02-02 22:21:39 -0800

    Input done

--- APCS-A-2020/src/Unit0/Input.java
+++ APCS-A-2020/src/Unit0/Input.java
@@ -25,9 +25,26 @@ public class Input
 		System.out.print("Enter an integer :: ");
 		intOne = keyboard.nextInt();
 
-
 		System.out.print("Enter an integer :: ");
 		intTwo = keyboard.nextInt();
+		
+		System.out.print("Emter a double:: ");
+		doubleOne = keyboard.nextDouble();
+
+		System.out.print("Emter a double:: ");
+		doubleTwo = keyboard.nextDouble();
+
+		System.out.print("Emter a float:: ");
+		floatOne = keyboard.nextFloat();
+		
+		System.out.print("Emter a float:: ");
+		floatTwo = keyboard.nextFloat();
+		
+		System.out.print("Emter a short:: ");
+		shortOne = keyboard.nextShort();
+		
+		System.out.print("Emter a short:: ");
+		shortTwo = keyboard.nextShort();
 
 
 		//add in input for all variables
@@ -36,6 +53,13 @@ public class Input
 		System.out.println();
 		System.out.println("integer one = " + intOne );
 		System.out.println("integer two = " + intTwo );
+		System.out.println("double  one = " + doubleOne );
+		System.out.println("double  two = " + doubleTwo );
+		System.out.println("float one = " + floatOne );
+		System.out.println("float two = " + floatTwo );
+		System.out.println("short one = " + shortOne );
+		System.out.println("short two = " + shortTwo );
+		
 
 		//add in output for all variables
 
